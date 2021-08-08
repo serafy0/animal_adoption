be6cd1f0a1504653170e2ee7a37c19224688d8ab
mohamedelserafy <elserafy02@gmail.com>
2021-08-08 20:40:44 +0200

    UI updates    -changed nav header

--- fronted/src/components/Nav.jsx
+++ fronted/src/components/Nav.jsx
@@ -1,16 +1,31 @@
-import { Link } from "react-router-dom";
-
+import {
+    HeaderNavigation,
+    ALIGN,
+    StyledNavigationItem as NavigationItem,
+    StyledNavigationList as NavigationList,
+} from 'baseui/header-navigation';
+import { StyledLink} from "baseui/link";
+import {Link} from "react-router-dom";
 const Nav = () => {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="sign-up">Sing Up</Link></li>
-                    <li><Link to="sign-in">Sign In</Link></li>
-                </ul>
-            </nav>
-        </header>
+
+    <HeaderNavigation>
+        <NavigationList $align={ALIGN.left}>
+            <NavigationItem>Animal Adoption</NavigationItem>
+        </NavigationList>
+        <NavigationList $align={ALIGN.center}>
+            <NavigationItem>
+                <StyledLink $as={Link} to="/about"to="/">Home</StyledLink>
+            </NavigationItem>
+            <NavigationItem>
+                <StyledLink $as={Link} to="/about" to="/sign-up">Sign up</StyledLink>
+            </NavigationItem>
+            <NavigationItem>
+                <StyledLink $as={Link} to="/about" to="/sign-in">sign in</StyledLink>
+            </NavigationItem>
+        </NavigationList>
+
+    </HeaderNavigation>
     )
 }
 export default Nav;
